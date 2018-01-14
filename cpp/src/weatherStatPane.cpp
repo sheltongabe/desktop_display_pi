3945ce41293eea7571e63aede73b835717d3a95c
sheltongabe <sheltongabe0@gmail.com>
2018-01-14 17:49:44 -0500

    Add Celsius to screen

--- cpp/src/weatherStatPane.cpp
+++ cpp/src/weatherStatPane.cpp
@@ -4,9 +4,11 @@ WeatherStatPane::WeatherStatPane() {
     //Create the widgets
     downSpeedLbl = new QLabel();
     farenheitLbl = new QLabel();
+    celsiusLbl = new QLabel();
 
     layout->addWidget(downSpeedLbl, 0, 0);
     layout->addWidget(farenheitLbl, 1, 0);
+    layout->addWidget(celsiusLbl, 1, 1);
     layout->setSpacing(0);
     layout->setMargin(0);
 
@@ -14,6 +16,7 @@ WeatherStatPane::WeatherStatPane() {
     styleMap = new StyleMap(StyleReader::read(":/styles/statPane.txt"));
     downSpeedLbl->setStyleSheet(styleMap->getStyle("downSpeedLbl"));
     farenheitLbl->setStyleSheet(styleMap->getStyle("farenheitLbl"));
+    celsiusLbl->setStyleSheet(styleMap->getStyle("celsiusLbl"));
 
     setLayout(layout);
 
@@ -22,6 +25,7 @@ void WeatherStatPane::updateWidgets() {
     //values for testing
     double downSpeed = 15.643;
     double tempFarenheit = 32.123;
+    double tempCelsius = 0.123;
 
     //setup file input
     QFile file(dataFileName);
@@ -34,16 +38,26 @@ void WeatherStatPane::updateWidgets() {
     tempFarenheit = dataFile.readLine().toDouble();
     file.close();
 
+    //convert the farhenheit temp to celsius
+    tempCelsius = (5 / 9) (tempFarenheit - 32);
+
     QString downText = "Internet Speed: ";
     downText.append(QString::number(downSpeed));
     downText.append("Mb/sec down");
 
-    QString tempText = "Temperature: ";
-    tempText.append(QString::number(tempFarenheit));
-    tempText.append(QChar(0260)).append("F");
+    //setup the temperature labels
+    QString tempFText = "Temperature: ";
+    tempFText.append(QString::number(tempFarenheit));
+    tempFText.append(QChar(0260)).append("F");
+
+    //setup the celsius labels
+    QString tempCText = " /  ";
+    tempCText.append(QString::number(tempCelsius));
+    tempCText.append(QChar(0260)).append("C");
 
     downSpeedLbl->setText(downText);
-    farenheitLbl->setText(tempText);
+    farenheitLbl->setText(tempFText);
+    celsiusLbl->setText(tempCText);
 }
 
 WeatherStatPane::~WeatherStatPane() {
