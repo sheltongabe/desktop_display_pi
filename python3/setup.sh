149cbc39fd78ec989fb7aaf955ce683d850164bf
Gabriel Shelton <sheltongabe0@gmail.com>
2018-01-15 21:03:05 -0500

    edit how to remove screen timer

--- python3/setup.sh
+++ python3/setup.sh
@@ -10,4 +10,7 @@ apt install pip3
 pip3 install requests
 pip3 install bs4
 
-#remove screen timeout and poweroff timeout with /etc/kbd/config
+#remove screen timeout and poweroff timeout with  /etc/lightdm/lightdm.conf
+#modify to 
+#[SeatDefaults]
+#xserver-command=X -s 0 -dpms
