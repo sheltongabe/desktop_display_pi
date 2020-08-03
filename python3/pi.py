fc0d3d84eee17a00f8775b00970b74d76b158c91
Gabriel Shelton <sheltongabe0@gmail.com>
2020-08-03 08:15:25 -0400

    Add Humiture Sensor & Fix instability (#3)
    * Split all items into their own threads
    
    * fix loop logic to wait timeout
    
    * Rework temperature sensor to be based on humiture sensor
    Added a timeout to the web requests
    
    * Fix errors from old project migration
    
    * Fix no check for None value for internal temp
    
    * Update link to test download speed and timeouts
    
    * Add error message for reading internal temperature

--- python3/pi.py
+++ python3/pi.py
@@ -1,11 +1,12 @@
-VALID_IMPORT = True;
+VALID_IMPORT = True
 try:
-    import PCF8591 as ADC;
-    import RPi.GPIO as GPIO;
-    import time, math;
+    import PCF8591 as ADC
+    import RPi.GPIO as GPIO
+    from humiture_sensor import HumitureSensor
+    import time, math
 except ImportError:
-    print("error installing Raspberry Pi Sensor Libraries, run setup.sh to install");
-    VALID_IMPORT = False;
+    print("error installing Raspberry Pi Sensor Libraries, run setup.sh to install")
+    VALID_IMPORT = False
 
 class PI:
     """pi.py
@@ -16,17 +17,18 @@ class PI:
         """ pi.init()
             constructor to initialize class
         """
-        self._valid_import = VALID_IMPORT;
+        self._valid_import = VALID_IMPORT
         
         #setup constants
-        self._DO = 17;
+        self._DO = 17
         if(self._valid_import):
-            GPIO.setmode(GPIO.BCM);
-            ADC.setup(0x48);
-            GPIO.setup(self._DO, GPIO.IN);
+            GPIO.setmode(GPIO.BCM)
+            ADC.setup(0x48)
+            GPIO.setup(self._DO, GPIO.IN)
+            self._humiture = HumitureSensor()
     
     def valid_import(self):
-        return self._valid_import;
+        return self._valid_import
 
     def getTemp(self):
         """ pi.getTemp()
@@ -34,14 +36,20 @@ class PI:
             returned by rasperrypi.
         """
         if(self.valid_import()):
-            return self.analogToTemp(ADC.read(0));
+            result = self._humiture.getHumidity()
+            if result:
+                humidity, temperature = result
+                return temperature
+            # return self.analogToTemp(ADC.read(0))
         else:
-            return None;
+            return None
+            print('Error Reading internal Temperature.')
+        return None
 
     def analogToTemp(self, analog):
-        Vr = 5 * float(analog) / 255;
-        Rt = 10000 * Vr / (5 - Vr);
-        temp = 1 / (((math.log(Rt / 10000)) / 3950) + (1 / (273.15 + 25)));
-        temp -= 273.15;
+        Vr = 5 * float(analog) / 255
+        Rt = 10000 * Vr / (5 - Vr)
+        temp = 1 / (((math.log(Rt / 10000)) / 3950) + (1 / (273.15 + 25)))
+        temp -= 273.15
 
-        return temp;
+        return temp
